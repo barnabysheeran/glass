07cfc055dd968228266f4507c5570aa6982390ed
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-06-29 15:13:17 +0100

    feat: add initialise methods and holder creation for Interactive and VideoSurface classes

--- src/js/interactive/Interactive.js
+++ src/js/interactive/Interactive.js
@@ -1,11 +1,22 @@
 import ApplicationLogger from '../application/ApplicationLogger.js';
 
+import Display from '../display/Display.js';
+
 export default class Interactive {
+	static #HOLDER;
+
 	static #LOG_LEVEL = 2;
 
 	// _________________________________________________________________________
 
 	static initialise() {
 		ApplicationLogger.log('Interactive', this.#LOG_LEVEL);
+
+		// Create Holder
+		this.#HOLDER = document.createElement('div');
+		this.#HOLDER.classList.add('interactive');
+
+		// Append Holder to Display Holder
+		Display.getDisplayHolder().appendChild(this.#HOLDER);
 	}
 }
