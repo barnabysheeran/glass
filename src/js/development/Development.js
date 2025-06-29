bc424a175d4562100819af436ceab2ff88e013e8
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-06-29 14:34:19 +0100

    feat: implement DevelopmentGrid and enhance Development class for improved functionality

--- src/js/development/Development.js
+++ src/js/development/Development.js
@@ -1,8 +1,16 @@
 import ApplicationLogger from '../application/ApplicationLogger.js';
 
+import Display from '../display/Display.js';
+
 import DevelopmentGuides from './guide/DevelopmentGuides.js';
+import DevelopmentGrid from './grid/DevelopmentGrid.js';
 
 export default class Development {
+	static #DEVELOPMENT_HOLDER;
+
+	static #DEVELOPMENT_GUIDES;
+	static #DEVELOPMENT_GRID;
+
 	static #LOG_LEVEL = 2;
 
 	// _________________________________________________________________________
@@ -10,7 +18,47 @@ export default class Development {
 	static initialise() {
 		ApplicationLogger.log('Development', this.#LOG_LEVEL);
 
+		// Create Development Container on Display Container
+		const DISPLAY_HOLDER = Display.getDisplayHolder();
+
+		// Create Development Container
+		this.#DEVELOPMENT_HOLDER = document.createElement('div');
+		this.#DEVELOPMENT_HOLDER.className = 'development';
+		DISPLAY_HOLDER.appendChild(this.#DEVELOPMENT_HOLDER);
+
 		// Create Development Guides
-		new DevelopmentGuides();
+		this.#DEVELOPMENT_GUIDES = new DevelopmentGuides(this.#DEVELOPMENT_HOLDER);
+
+		// Create Development Grid
+		this.#DEVELOPMENT_GRID = new DevelopmentGrid(this.#DEVELOPMENT_HOLDER);
+
+		// Add Keyboard Event Listener
+		window.addEventListener('keyup', this.#onKeyUp.bind(this));
+	}
+
+	// _________________________________________________________________________
+
+	static #onKeyUp(event) {
+		// 1 - Development Guides Toggle
+		if (event.keyCode === 49) {
+			this.#toggleGuidesShowHide();
+		}
+
+		// 2 - Development Grid Toggle
+		if (event.keyCode === 50) {
+			this.#toggleGridShowHide();
+		}
+	}
+
+	// ___________________________________________________________________ Guide
+
+	static #toggleGuidesShowHide() {
+		this.#DEVELOPMENT_GUIDES.toggleShowHide();
+	}
+
+	// ____________________________________________________________________ Grid
+
+	static #toggleGridShowHide() {
+		this.#DEVELOPMENT_GRID.toggleShowHide();
 	}
 }
