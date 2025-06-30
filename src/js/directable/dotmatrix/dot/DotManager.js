7462df647a3b2d2d3f1ffc588c467aad5ea06977
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-06-30 13:39:10 +0100

    fix: reduce dot pool size in DotManager for optimized resource management

--- src/js/directable/dotmatrix/dot/DotManager.js
+++ src/js/directable/dotmatrix/dot/DotManager.js
@@ -6,7 +6,7 @@ import Dot from './Dot.js';
 
 export default class DotManager {
 	#DOTS = [];
-	#dotPoolSize = 10000;
+	#dotPoolSize = 1000;
 	#dotPoolIndex = 0;
 
 	#LOG_LEVEL = 4;
@@ -28,13 +28,18 @@ export default class DotManager {
 	// ________________________________________________________________ Dot Pool
 
 	getNextFreeDotIndex() {
+		// Store Index for Return
 		let index = this.#dotPoolIndex;
+
+		// console.log('DotManager index ' + index);
 
 		// Next
 		this.#dotPoolIndex += 1;
 
 		// Recycle from Start of Pool
 		if (this.#dotPoolIndex >= this.#dotPoolSize) {
+			console.log(`DotManager Reached end of dot pool. Resetting index to 0.`);
+
 			this.#dotPoolIndex = 0;
 		}
 
