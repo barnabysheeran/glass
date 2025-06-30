313dd07775d2279c2b7ffea17e118f5df7e682ab
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-06-30 11:30:15 +0100

    Refactor code structure for improved readability and maintainability

--- src/js/directable/dotmatrix/dot/DotManager.js
+++ src/js/directable/dotmatrix/dot/DotManager.js
@@ -6,7 +6,7 @@ import Dot from './Dot.js';
 
 export default class DotManager {
 	#DOTS = [];
-	#dotPoolSize = 10240;
+	#dotPoolSize = 1000;
 	#dotPoolIndex = 0;
 
 	#LOG_LEVEL = 4;
--- src/js/directable/dotmatrix/view/ViewTest.js
+++ src/js/directable/dotmatrix/view/ViewTest.js
@@ -17,27 +17,39 @@ export default class ViewTest {
 		this.#COMPONENT_MANAGER = componentManager;
 
 		// Create Components
-		this.#COMPONENT_MANAGER.addComponentLineWidthFull(1);
+		// this.#COMPONENT_MANAGER.addComponentLineWidthFull(1);
 
 		this.#COMPONENT_MANAGER.addComponentTextBox(
-			'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-			1,
-			6,
+			'A',
+			10,
+			10,
 			100,
 			50,
 			FillType.PassThrough,
 			FillStrategyType.PassThrough,
 		);
 
-		this.#COMPONENT_MANAGER.addComponentTextBox(
-			'AAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-			1,
-			15,
-			100,
-			50,
-			FillType.PassThrough,
-			FillStrategyType.PassThrough,
-		);
+		// this.#COMPONENT_MANAGER.addComponentTextBox(
+		// 	'AAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+		// 	1,
+		// 	15,
+		// 	100,
+		// 	50,
+		// 	FillType.PassThrough,
+		// 	FillStrategyType.PassThrough,
+		// );
+
+		// for (let i = 0; i < 10; i++) {
+		// 	this.#COMPONENT_MANAGER.addComponentTextBox(
+		// 		'XXXXXXXXXXXXXXXXXXXXX',
+		// 		1,
+		// 		24 + i * 7,
+		// 		100,
+		// 		50,
+		// 		FillType.PassThrough,
+		// 		FillStrategyType.PassThrough,
+		// 	);
+		// }
 	}
 
 	// ____________________________________________________________________ tick
