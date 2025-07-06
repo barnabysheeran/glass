c737538063108c328ab782a63c0bdd9c5ef736f3
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-07-06 06:58:11 +0100

    refactor: enhance project menu logic for grid size handling and text selection

--- src/js/directable/dotmatrix/view/view/DotMatrixViewProjectMenu.js
+++ src/js/directable/dotmatrix/view/view/DotMatrixViewProjectMenu.js
@@ -15,6 +15,9 @@ export default class DotMatrixViewProjectMenu extends DotMatrixView {
 	// TODO Constants
 	#LINE_HEIGHT = 7;
 
+	// TODO Calc Pixels
+	#BLOCK_WIDTH_MOBILE = 130;
+
 	#LOG_LEVEL = 4;
 
 	// _________________________________________________________________________
@@ -28,6 +31,13 @@ export default class DotMatrixViewProjectMenu extends DotMatrixView {
 	// ___________________________________________________________________ Start
 
 	start() {
+		// Get Grid Size
+		const GRID_MAX = GridData.getGridMax();
+
+		const GRID_MAX_WIDTH = GRID_MAX[0];
+
+		console.log('GRID_MAX_WIDTH', GRID_MAX_WIDTH);
+
 		// Get Project Data
 		const PROJECT_DATA = DataController.getProjects();
 
@@ -35,9 +45,15 @@ export default class DotMatrixViewProjectMenu extends DotMatrixView {
 		for (let i = 0; i < PROJECT_DATA.length; i += 1) {
 			const PROJECT_DATA_ITEM = PROJECT_DATA[i];
 
+			let text = PROJECT_DATA_ITEM['name'];
+
+			if (GRID_MAX_WIDTH < this.#BLOCK_WIDTH_MOBILE) {
+				text = PROJECT_DATA_ITEM['name-short'];
+			}
+
 			const COMPONENT = new ComponentGlyphBox(
 				this.SHAPE_MANAGER,
-				PROJECT_DATA_ITEM['name'],
+				text,
 				8,
 				this.#LINE_HEIGHT * (7 + i * 2),
 				100,
